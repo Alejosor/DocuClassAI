81931f3cf7267d0b9fb9e9f98a169971b4719d04
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-05-15 11:54:16 -0500

    Sistema funcional para subir y mostrar texto plano.

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,22 @@
 from flask import render_template, request, redirect, url_for
 from app import app
+from app.utils import extract_text_from_pdf
+import os
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         uploaded_file = request.files.get('file')
         if uploaded_file:
-            uploaded_file.save(f'uploads/{uploaded_file.filename}')
+            upload_path = os.path.join('uploads', uploaded_file.filename)
+            uploaded_file.save(upload_path)
             return redirect(url_for('result', filename=uploaded_file.filename))
     return render_template('index.html')
 
 @app.route('/resultado/<filename>')
 def result(filename):
-    return render_template('result.html', filename=filename)
+    file_path = os.path.join('uploads', filename)
+    content = extract_text_from_pdf(file_path)
+    
+    # Por ahora, sin clasificar, solo mostramos el texto
+    return render_template('result.html', filename=filename, content=content)
